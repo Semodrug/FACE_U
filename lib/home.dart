943d37bc24bc172ae972d02e127a666c533ef8a0
EunyoungY <43934072+EunyoungY@users.noreply.github.com>
2020-11-20 12:44:35 +0900

    bottom appbar & appbar

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,64 @@
 import 'package:flutter/material.dart';
 
+import 'camera.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions =
+      <Widget>[
+        Text('Index 0: Home'),
+//        Text('Index 1: Camera'),
+        Text('Index 2: Plan'),
+      ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text("FaceU", style: Theme.of(context).textTheme.headline1)
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_enhance),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(
+                    builder: (context) => Camera()
+              ));
+        },
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'home'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: 'plan'
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme.of(context).bottomAppBarColor,
+        onTap: _onItemTapped
+      ),
+
+    );
   }
 }
--- lib/route.dart
+++ lib/route.dart
@@ -9,25 +9,37 @@ import 'recognition.dart';
 
 
 class FaceUApp extends StatelessWidget {
+  Color color = const Color(0xffFFdb80);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Shrine',
         home: HomePage(),//HomePage
-        initialRoute: '/login',
+//        initialRoute: '/login',
+        initialRoute: '/home',
         routes: {
           '/login': (context) => LogInPage(),
           '/home': (context) => HomePage(),
           '/add': (context) => AddPage(),
           '/recognition': (context) => Recognition(),
           '/plan': (context) => Plan(),
+        },
+      theme: ThemeData(
 
-
-//          Profile.routeName: (context) =>
-//              Profile(),
-
-        }
+        primaryColor: Color(0xffFFdb80),
+        bottomAppBarColor: Color(0xffFFCB83),
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 20.0, fontFamily: 'OmniGothic',
+              color: Colors.black87, fontWeight: FontWeight.bold), //apbar feature
+          headline2: TextStyle(fontSize: 13.0, fontFamily: 'NotoSans',
+              color: Colors.black87, fontWeight: FontWeight.bold),
+//          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind',
+          color: Colors.black54),
+        ),
+      )
       //onGenerateRoute: _getRoute,
     );
   }
