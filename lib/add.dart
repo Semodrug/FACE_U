1e41709b19bfb151b0652757a63493d0ee7fe4a8
Sumi Lee <40039201+underhiswings@users.noreply.github.com>
2020-11-25 00:49:41 +0900

    Update add.dart

--- lib/add.dart
+++ lib/add.dart
@@ -11,10 +11,12 @@ class _AddPageState extends State<AddPage> {
     TextEditingController _nameCtl = TextEditingController();
     TextEditingController _priceCtl = TextEditingController();
     TextEditingController _descriptionCtl = TextEditingController();
+    TextEditingController _characterCtl = TextEditingController();
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
+        elevation: 0,
         leading: TextButton(
           child: Text(
             '취소',
@@ -28,7 +30,7 @@ class _AddPageState extends State<AddPage> {
           },
         ),
         title: Text(
-          '페이스 추',
+          '페이스 추가',
         ),
         centerTitle: true,
         actions: <Widget>[
@@ -36,8 +38,8 @@ class _AddPageState extends State<AddPage> {
             child: Text(
               '완료',
               maxLines: 1,
-              // TODO: controller의 내용이 차있으면 색깔 다르게
-              style: TextStyle(color: Colors.white),
+              // TODO: controller의 내용이 차있으면 blue, 없으면 gray
+              style: TextStyle(color: Colors.blue),
             ),
             onPressed: () async {
               // TODO: 완료되었다는 메세지와 함께 자동으로 home으로 가게끔
@@ -60,42 +62,64 @@ class _AddPageState extends State<AddPage> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              AspectRatio(
-                  aspectRatio: 18 / 11,
-                  child: Image.network(
-                      'http://handong.edu/site/handong/res/img/logo.png')
-                  //Image.file(image),
-                  ),
+              SizedBox(
+                height: 25,
+              ),
+              Container(
+                  width: 135.0,
+                  height: 135.0,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: NetworkImage(
+                              "https://i.imgur.com/BoN9kdC.png")))),
               Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  IconButton(
-                      icon: Icon(
-                        Icons.camera_alt,
-                        color: Colors.black,
-                      ),
-                      onPressed: () {
-                        // getImage();
-                      }),
+                  FlatButton(
+                    child: Text(
+                      '사진 추가',
+                    ),
+                    onPressed: () => null,
+                    textColor: Colors.blue,
+                  ),
                 ],
               ),
+              SizedBox(
+                height: 20,
+              ),
+              // TODO: hint theme
               TextField(
                 controller: _nameCtl,
                 decoration: InputDecoration(
-                    hintText: 'Product Name',
-                    hintStyle: TextStyle(color: Colors.blue[700])),
+                  hintText: '이름을 입력해주세요',
+                  // hintStyle: TextStyle(color: Colors.blue[700])
+                ),
               ), // border: OutlineInputBorder()
               TextField(
                 controller: _priceCtl,
                 decoration: InputDecoration(
-                    hintText: 'Price',
-                    hintStyle: TextStyle(color: Colors.blue[700])),
+                  hintText: '소속을 입력해주세요 (가족, 친구, 직장 등)',
+                  // hintStyle: TextStyle(color: Colors.blue[700])
+                ),
               ),
               TextField(
                 controller: _descriptionCtl,
                 decoration: InputDecoration(
-                    hintText: 'Description',
-                    hintStyle: TextStyle(color: Colors.blue[700])),
+                  hintText: '관계를 입력해주세요 (엄마, 대학친구, 상사 등)',
+                  // hintStyle: TextStyle(color: Colors.blue[700])
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: _characterCtl,
+                decoration: InputDecoration(
+                  hintText: '특징을 입력해주세요',
+                  // hintStyle: TextStyle(color: Colors.blue[700])
+                ),
               ),
             ],
           ),
