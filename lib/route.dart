f93d7bf1299e94913e5e6434ab8451881134b710
Sumi Lee <40039201+underhiswings@users.noreply.github.com>
2020-11-26 08:17:08 +0900

    Add result.dart

--- lib/route.dart
+++ lib/route.dart
@@ -7,17 +7,16 @@ import 'home.dart';
 import 'add.dart';
 import 'plan.dart';
 import 'recognition.dart';
-
+import 'result.dart';
 
 class FaceUApp extends StatelessWidget {
   Color color = const Color(0xffFFdb80);
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Shrine',
-        home: HomePage(),//HomePage
+        home: HomePage(), //HomePage
 //        initialRoute: '/login',
 //        initialRoute: '/home',
         initialRoute: '/camera',
@@ -28,29 +27,40 @@ class FaceUApp extends StatelessWidget {
           '/recognition': (context) => Recognition(),
           '/plan': (context) => Plan(),
           '/camera': (context) => Camera(),
+          '/result': (context) => ResultPage(),
         },
-      theme: ThemeData(
-
-        primaryColor: Color(0xffFFdb80),
-
-        bottomAppBarColor: Color(0xffFFCB83),
-        textTheme: TextTheme(
-          headline1: TextStyle(fontSize: 20.0, fontFamily: 'OmniGothic',
-              color: Colors.black87, fontWeight: FontWeight.bold), //apbar feature
-          headline2: TextStyle(fontSize: 13.0, fontFamily: 'NotoSans',
-              color: Colors.black87, fontWeight: FontWeight.bold),
+        theme: ThemeData(
+          primaryColor: Color(0xffFFdb80),
+          bottomAppBarColor: Color(0xffFFCB83),
+          textTheme: TextTheme(
+            headline1: TextStyle(
+                fontSize: 20.0,
+                fontFamily: 'OmniGothic',
+                color: Colors.black87,
+                fontWeight: FontWeight.bold), //apbar feature
+            headline2: TextStyle(
+                fontSize: 13.0,
+                fontFamily: 'NotoSans',
+                color: Colors.black87,
+                fontWeight: FontWeight.bold),
 //          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
 //          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind',
 //          color: Colors.black54),
-          bodyText1: TextStyle(fontSize: 14.0, fontFamily: 'NotoSans',
-              color: Colors.black54, ),
-          bodyText2: TextStyle(fontSize: 12.0, fontFamily: 'NotoSans',
-            color: Colors.black54, ),
-        ),
-      )
-      //onGenerateRoute: _getRoute,
-    );
+            bodyText1: TextStyle(
+              fontSize: 14.0,
+              fontFamily: 'NotoSans',
+              color: Colors.black54,
+            ),
+            bodyText2: TextStyle(
+              fontSize: 12.0,
+              fontFamily: 'NotoSans',
+              color: Colors.black54,
+            ),
+          ),
+        )
+        //onGenerateRoute: _getRoute,
+        );
   }
 
   Route<dynamic> _getRoute(RouteSettings settings) {
