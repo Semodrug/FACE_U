6ae48154dbfe3c2320cc4e25329f199a96daa330
EunyoungY <43934072+EunyoungY@users.noreply.github.com>
2020-11-26 03:52:34 +0900

    update camera page

--- lib/camera.dart
+++ lib/camera.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'demo.dart';
 
 CameraState pageState;
 
