e3d585169508fe8a607d1f8b4374ae3577c91d89
Sumi Lee <40039201+underhiswings@users.noreply.github.com>
2020-12-10 20:41:49 +0900

    Update camera.dart

--- lib/camera.dart
+++ lib/camera.dart
@@ -1,13 +1,4 @@
-import 'dart:io';
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
-
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image_picker/image_picker.dart';
-
-import 'package:face_u/face_detection.dart';
-import 'package:face_u/barcode.dart';
 
 CameraState pageState;
 
@@ -20,43 +11,6 @@ class Camera extends StatefulWidget {
 }
 
 class CameraState extends State<Camera> {
-//  File _image;
-  File galleryImage;
-  FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-  User _user;
-  FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
-  String _profileImageURL = "";
-
-
-
-  @override
-  void initState() {
-    super.initState();
-    _prepareService();
-  }
-
-  void _prepareService() async {
-    _user = await _firebaseAuth.currentUser;
-  }
-
-  void _uploadImageToStorage(ImageSource source) async {
-    File image = await ImagePicker.pickImage(source: source);
-
-    if (image == null) return;
-    setState(() {
-      galleryImage = image;
-    });
-
-    Reference storageReference = _firebaseStorage.ref().child("profile");
-
-    UploadTask storageUploadTask = storageReference.putFile(galleryImage);
-
-    String downloadURL = await storageReference.getDownloadURL();
-    setState(() {
-      _profileImageURL = downloadURL;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return SimpleDialog(
@@ -64,23 +18,13 @@ class CameraState extends State<Camera> {
         SimpleDialogOption(
           child: Text('얼굴 인식하기'),
           onPressed: () {
-            _uploadImageToStorage(ImageSource.gallery);
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => FaceDetection(image: galleryImage)));
+            Navigator.pushNamed(context, '/face_detection');
           },
         ),
         SimpleDialogOption(
           child: Text('바코드 인식하기'),
           onPressed: () {
-            _uploadImageToStorage(ImageSource.gallery);
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-//                    builder: (context) => ReadBarcode(text: text)));
-                    builder: (context) => ReadBarcode()));
-
+            Navigator.pushNamed(context, '/barcode');
           },
         ),
         SimpleDialogOption(child: Text('취소'), onPressed: () {}),
