5074123228b68a2e8e505c906d01dbf3c6330fbb
Sumi Lee <40039201+underhiswings@users.noreply.github.com>
2020-12-11 17:35:12 +0900

    Update a camera detection

--- lib/barcode.dart
+++ lib/barcode.dart
@@ -4,11 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
-
 class ReadBarcode extends StatefulWidget {
   @override
   _ReadBarcodeState createState() => _ReadBarcodeState();
-
 
 /*  final File image;
   const ReadBarcode({Key key, this.image}) : super(key: key);
@@ -29,9 +27,8 @@ class _ReadBarcodeState extends State<ReadBarcode> {
 /*  final String text;
   _ReadBarcodeState(this.text);*/
 
-
   File pickedImage;
-  var text = '';
+  var text = '바코드를 인식해주세요';
 
   bool imageLoaded = false;
 
@@ -47,24 +44,21 @@ class _ReadBarcodeState extends State<ReadBarcode> {
     VisionText readedText;
 
     final BarcodeDetector barcodeDetector =
-    FirebaseVision.instance.barcodeDetector();
+        FirebaseVision.instance.barcodeDetector();
 
     final List<Barcode> barcodes =
-    await barcodeDetector.detectInImage(visionImage);
+        await barcodeDetector.detectInImage(visionImage);
 
     for (Barcode barcode in barcodes) {
-
       final String rawValue = barcode.rawValue;
       final BarcodeValueType valueType = barcode.valueType;
 
       setState(() {
-        text ="$rawValue";
+        text = "$rawValue";
       });
-
     }
     barcodeDetector.close();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -74,62 +68,69 @@ class _ReadBarcodeState extends State<ReadBarcode> {
               style: Theme.of(context).textTheme.headline1)),
       body: Column(
         children: <Widget>[
-          SizedBox(height: 100.0),
           imageLoaded
               ? Center(
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: const [
-                        BoxShadow(blurRadius: 20),
-                      ],
-                    ),
-                    margin: EdgeInsets.fromLTRB(0, 0, 0, 8),
-                    height: 250,
-                    child: Image.file(
-                      pickedImage,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  SizedBox(height: 20.0),
-                  Text("Barcode Number:  "+text),
-                ],
-              ))
-              : Container(
-            child: Center(
-              child: InkWell(
-                child: Container(
-                  height: 150,
-                  width: 200,
-//                  color: Colors.grey,
                   child: Column(
-                    children: [
-                      Icon(Icons.photo_camera, size: 100),
-                      SizedBox(height: 10,),
-                      Text('바코드 인식', style: TextStyle(color: Colors.blue, fontSize: 15),)
-                    ],
-                  ),
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(blurRadius: 20),
+                        ],
+                      ),
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                      height: 250,
+                      child: Image.file(
+                        pickedImage,
+                        // fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ))
+              : Container(),
+          SizedBox(height: 20.0),
+          Row(children: [
+            SizedBox(width: 10),
+            InkWell(
+              child: Container(
+                height: 100,
+                width: 100,
+//                  color: Colors.grey,
+                child: Column(
+                  children: [
+                    Icon(Icons.photo_camera, size: 60),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      '바코드 인식',
+                      style: TextStyle(color: Colors.blue, fontSize: 15),
+                    ),
+                  ],
                 ),
-                onTap: () async{
-                  pickImage();
-
-                  },
               ),
+              onTap: () async {
+                pickImage();
+              },
+            ),
+            SizedBox(width: 10),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("결과"),
+                SizedBox(height: 10.0),
+                Text("Barcode Number:  " + text),
+                SizedBox(height: 30.0),
+              ],
             )
-          ),
+          ]),
         ],
       ),
     );
   }
 }
-
-
-
-
-
-
 
 /*import 'dart:io';
 
@@ -139,13 +140,13 @@ import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
 
 class ReadBarcode extends StatefulWidget {
-*//*  @override
-  _ReadBarcodeState createState() => _ReadBarcodeState();*//*
+*/ /*  @override
+  _ReadBarcodeState createState() => _ReadBarcodeState();*/ /*
 
-*//*  final File image;
+*/ /*  final File image;
   const ReadBarcode({Key key, this.image}) : super(key: key);
   @override
-  _ReadBarcodeState createState() => _ReadBarcodeState(image);*//*
+  _ReadBarcodeState createState() => _ReadBarcodeState(image);*/ /*
 
   final String text;
   const ReadBarcode({Key key, this.text}) : super(key: key);
@@ -154,8 +155,8 @@ class ReadBarcode extends StatefulWidget {
 }
 
 class _ReadBarcodeState extends State<ReadBarcode> {
-*//*  final File image;
-  _ReadBarcodeState(this.image);*//*
+*/ /*  final File image;
+  _ReadBarcodeState(this.image);*/ /*
 
   final String text;
   _ReadBarcodeState(this.text);
@@ -166,7 +167,7 @@ class _ReadBarcodeState extends State<ReadBarcode> {
 //  var text = '';
 //  bool imageLoaded = false;
 
-*//*  Future pickImage() async {
+*/ /*  Future pickImage() async {
 //    var awaitImage = await ImagePicker.pickImage(source: ImageSource.gallery);
 
 //    setState(() {
@@ -195,7 +196,7 @@ class _ReadBarcodeState extends State<ReadBarcode> {
 
     }
     barcodeDetector.close();
-  }*//*
+  }*/ /*
 
 
   @override
@@ -208,7 +209,7 @@ class _ReadBarcodeState extends State<ReadBarcode> {
               Center()
           :
           Text("pick image: "+ text),
-          *//*Center(
+          */ /*Center(
               child: Container(
                 decoration: BoxDecoration(
                   color: Colors.white,
@@ -222,7 +223,7 @@ class _ReadBarcodeState extends State<ReadBarcode> {
 //                  pickedImage,
 //                  fit: BoxFit.cover,
 //                ),
-              )),*//*
+              )),*/ /*
           SizedBox(height: 30.0),
           Text("pick image: "+ text),
           SizedBox(height: 30.0),
@@ -239,6 +240,3 @@ class _ReadBarcodeState extends State<ReadBarcode> {
     );
   }
 }*/
-
-
-
